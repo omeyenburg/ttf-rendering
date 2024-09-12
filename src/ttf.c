1571ce5a535e146182bd8add172230b8d5f90421
omeyenburg <oskar.meyenburg@gmail.com>
2024-09-12 10:09:20 +0200

    Header table interpretation

--- src/ttf.c
+++ src/ttf.c
@@ -2,6 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define bytes4xint(buffer, offset)                                                     \
+    (((unsigned char) buffer[offset] << 24) |                                          \
+     ((unsigned char) buffer[offset + 1] << 16) |                                      \
+     ((unsigned char) buffer[offset + 2] << 8) | ((unsigned char) buffer[offset + 3]))
+
+#define bytes2xint(buffer, offset)                                                     \
+    (((unsigned char) buffer[offset] << 8) | ((unsigned char) buffer[offset + 1]))
+
+char* bytes4xchar(unsigned char* buffer, int offset) {
+    static char string[5];
+    string[0] = buffer[offset];
+    string[1] = buffer[offset + 1];
+    string[2] = buffer[offset + 2];
+    string[3] = buffer[offset + 3];
+    string[4] = '\0';
+    return string;
+}
+
 void load(char* path) {
     FILE* fp;
     size_t size;
@@ -40,9 +58,30 @@ void load(char* path) {
         exit(1);
     } else {
         for (size_t i = 0; i < size; i++) {
+            char c = buffer[i];
             printf("%02x ", buffer[i]);
+            // printf("%c ", c);
         }
         printf("\n"); // Print a newline for better formatting
+    }
+
+    long version = bytes4xint(buffer, 0);
+    printf("Version: %li\n", version);
+
+    long num_tables = bytes2xint(buffer, 4);
+    printf("Num tables: %li\n", num_tables);
+
+    int offset = 12;
+    for (int i = 0; i < num_tables; i++) {
+        const char* label = bytes4xchar(buffer, offset);
+        unsigned long check_sum = bytes4xint(buffer, offset + 4);
+        unsigned long boffset = bytes4xint(buffer, offset + 8);
+        unsigned long length = bytes4xint(buffer, offset + 12);
+        printf("Label: %s\n", label);
+        printf("Check sum: %lu\n", check_sum);
+        printf("Offset: %lu\n", boffset);
+        printf("Length: %lu\n", length);
+        offset += 16;
     }
 
     fclose(fp);
